871d552da44f7e6a04ef88e5a767f681d6d4a5fb
karandeep-3154 <ksingh310504@gmail.com>
2023-07-15 21:07:47 +0530

    Designing Homepage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import hot_weather_image from "./Images/hot_weather.webp";
 import cold_weather_image from "./Images/cold_weather.jpg";
+import cloud_image from "./Images/Clouds.webp";
 function App() {
   return (
     <div
@@ -13,6 +14,19 @@ function App() {
         <input type="text" name="city" placeholder="Enter the city Name" />
         <button>℉</button>
       </div>
+      <div className="section section-temperature">
+        <div className="icon">
+          <h4>New Delhi, IN</h4>
+          <img src={cloud_image} alt="" />
+          <h3>Cloudy</h3>
+        </div>
+        <div className="temperature">
+          <h1>35℃</h1>
+        </div>
+      </div>
+
+
+      
     </div>
    </div>
     </div>
