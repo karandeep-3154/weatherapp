08b4b0a1f840ba5447087859544bc7a89cb4f865
karandeep-3154 <ksingh310504@gmail.com>
2023-07-15 18:52:56 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import hot_weather_image from "./Images/hot_weather.webp";
+import cold_weather_image from "./Images/cold_weather.jpg";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="app"
+      style={{ backgroundImage: `url(${cold_weather_image})` }}
+    >
+      <h1>helloo jio</h1>f
+      .classNa
     </div>
   );
 }
